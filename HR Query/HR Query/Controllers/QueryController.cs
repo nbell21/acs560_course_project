57505a814677d6dbf5119e30069a8814f364b074
nbell21 <nathanandrewbell@gmail.com>
2013-11-13 07:29:58 -0500

    Course Project

---
+++ HR Query/HR Query/Controllers/QueryController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HR_Query.Models;
+
+namespace HR_Query.Controllers
+{
+    public class QueryController : Controller
+    {
+        
+        public ActionResult Index(string returnUrl)
+        {
+            using(var db = new HR_QueryEntities())
+            {            
+            var model = new QueryModel();
+
+            model.OptionsDropDown.Add(new SelectListItem { Text = "Select an option...", Value = "0" });
+
+            model.OptionsDropDown.Add(new SelectListItem { Text = "Query by department", Value = "1" });
+
+            model.OptionsDropDown.Add(new SelectListItem { Text = "Query by location", Value = "2" });
+
+            model.OptionsDropDown.Add(new SelectListItem { Text = "Query by position type", Value = "3" });
+
+            var myList = new List<SelectListItem>();
+
+            var list = db.Departments.ToList();
+
+            var items = from g in list
+                        select new SelectListItem
+                        {
+                            Value = g.Dept_ID.ToString(),
+                            Text = g.Dept_Name
+                        };
+
+            model.DepartmentsDropDown.Add(new SelectListItem { Text = "Select an option...", Value = "0" });
+
+            foreach (var item in items)
+            {
+                model.DepartmentsDropDown.Add(item);
+            }
+
+            return View(model);
+            }
+        }
+    }
+}
---
+++ HR Query/HR Query/Models/QueryModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HR_Query.Models
+{
+    public class QueryModel
+    {
+        public List<SelectListItem> OptionsDropDown { get; set; }
+        public String OptionsDropDownSelection { get; set; }
+        public List<SelectListItem> DepartmentsDropDown { get; set; }
+        public String DepartmentsDropDownSelection { get; set; }
+
+        public QueryModel()
+        {
+            OptionsDropDown = new List<SelectListItem>();
+            DepartmentsDropDown = new List<SelectListItem>();
+        }
+
+    }
+}
